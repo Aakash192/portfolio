f7780d9b1a63cb76aab7b906992c9dec3934bf43
Aakash192 <145707623+Aakash192@users.noreply.github.com>
2025-04-15 00:25:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ const projects = [
     {
     id: 8,
     title: "PID Diagram Document Analysis using ML & CV",
-    description: "Automated the extraction of components and text from P&ID diagrams using YOLOv8 and NLP. Achieved 98% detection accuracy and deployed the system on Azure using Docker, Kubernetes, and Prometheus.",
+    description: "Automated the extraction of components and text from P&ID diagrams using YOLOv8 and NLP. Achieved 98% detection accuracy.",
     image: "https://images.unsplash.com/photo-1629904853716-f0bc54eea481?auto=format&fit=crop&w=1350&q=80",
     technologies: ["YOLOv8", "NLP", "Machine Learning"],
     githubUrl: "https://github.com/aakashsuryavanshi/pid-diagram-analysis"
