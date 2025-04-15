d8d457657c23cae54e4a7b0563d37b310128d87b
Aakash192 <145707623+Aakash192@users.noreply.github.com>
2025-04-14 19:14:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ const projects = [
     },
     {
         id: 5,
-        title: "Container Orchestration Platform",
-        description: "Implemented Kubernetes cluster for microservices deployment, improving scalability and reducing downtime by 40%.",
-        image: "https://images.unsplash.com/photo-1667372393119-3d4c48d07fc9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-        technologies: ["Kubernetes", "Docker", "DevOps"],
-        githubUrl: "https://github.com/aakashsuryavanshi/k8s-platform"
+        title: "Chest X-Ray Classification using CNN and Computer Vision",
+        description: "In this project, I built a chest X-ray classification system using transfer learning with a pretrained ResNet18 to distinguish NORMAL from PNEUMONIA cases. I applied data augmentation and normalization on a well-structured dataset, fine-tuning the model with a batch size of 4 over 10 epochs. The model achieved a test accuracy of 95%, and I implemented a visualization tool to display test images alongside their predicted and true labels.",
+        image: "https://images.unsplash.com/photo-1581090700227-1e8b1f94f533?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        technologies: ["Machine Learning Model", "Supervised learning", "Computer Vision"],
+        githubUrl: "https://github.com/Aakash192/chest_xray_classification"
     },
     {
         id: 6,
@@ -53,7 +53,7 @@ const projects = [
         title: "Chest X-Ray Classification using CNN and Computer Vision",
         description: "In this project, I built a chest X-ray classification system using transfer learning with a pretrained ResNet18 to distinguish NORMAL from PNEUMONIA cases. I applied data augmentation and normalization on a well-structured dataset, fine-tuning the model with a batch size of 4 over 10 epochs. The model achieved a test accuracy of 95%, and I implemented a visualization tool to display test images alongside their predicted and true labels.",
         image: "https://images.unsplash.com/photo-1581090700227-1e8b1f94f533?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-        technologies: ["Machine Learning Model", "Supervised learning", "Computer Vision", ""],
+        technologies: ["Machine Learning Model", "Supervised learning", "Computer Vision"],
         githubUrl: "https://github.com/Aakash192/chest_xray_classification"
     }
 ];
