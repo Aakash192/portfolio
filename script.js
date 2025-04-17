3a22f0606c31bb3ff9192db7646a88e9e23365d3
Aakash192 <145707623+Aakash192@users.noreply.github.com>
2025-04-17 14:22:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -69,6 +69,9 @@ const projects = [
 
 // Function to create project cards
 function createProjectCard(project) {
+    // List of project IDs where the GitHub link should be hidden
+    const excludedIds = [1, 4];
+
     return `
         <div class="project-card">
             <img src="${project.image}" alt="${project.title}">
@@ -78,12 +81,29 @@ function createProjectCard(project) {
                 <div class="technologies">
                     ${project.technologies.map(tech => `<span class="tech-tag">${tech}</span>`).join('')}
                 </div>
-                <div class="project-links">
-                    <a href="${project.githubUrl}" target="_blank" class="github-link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
-                        View on GitHub
-                    </a>
-                </div>
+
+                ${
+                    // Only show GitHub link if the ID is NOT in the excluded list
+                    !excludedIds.includes(project.id) && project.githubUrl
+                    ? `<div class="project-links">
+                        <a href="${project.githubUrl}" target="_blank" class="github-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                 stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5
+                                         .08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35
+                                         0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8
+                                         0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0
+                                         3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5
+                                         6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22
+                                         1.23-.15 1.85v4"/>
+                                <path d="M9 18c-4.51 2-5-2-7-2"/>
+                            </svg>
+                            View on GitHub
+                        </a>
+                    </div>`
+                    : '' // If it's excluded, show nothing here
+                }
             </div>
         </div>
     `;
